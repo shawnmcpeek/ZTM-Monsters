493428390e44e0ba1fed3e1c50e702cb6380a056
Shawn McPeek <shawnmcpeek@gmail.com>
2024-01-15 21:45:45 -0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   public/index.html
    	modified:   src/App.js
    	new file:   src/components/card-list/card-list.component.jsx
    	new file:   src/components/search-box/search-box.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { name: "Linda" }, 
-        { name: "Frank" }, 
-        { name: "Jacky" },
-        { name: "Shawn" },
-        { name: "Sandi" },
-        { name: "Patrick" },]
+      monsters: [],
+      searchField: "", // Added a state property for the search field
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (event) => {
+    const searchString = event.target.value.toLowerCase();
+    this.setState({ searchField: searchString });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField)
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.name}>{monster.name}</h1>;
-        })}
+        <SearchBox
+          className="search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
