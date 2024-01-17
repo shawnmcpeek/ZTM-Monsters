2ddfba9914768418073d444da8f410811c8f8fe6
Shawn McPeek <shawnmcpeek@gmail.com>
2024-01-16 18:14:47 -0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   public/index.html
    	modified:   src/App.css
    	modified:   src/App.js
    	modified:   src/components/card-list/card-list.component.jsx
    	new file:   src/components/card-list/card-list.styles.css
    	new file:   src/components/card/card.component.jsx
    	new file:   src/components/card/card.styles.css
    	modified:   src/components/search-box/search-box.component.jsx
    	new file:   src/components/search-box/search-box.styles.css

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           className="search-box"
           onChangeHandler={onSearchChange}
